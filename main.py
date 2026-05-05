42b6abd385c1270a2e093cb62bf7d704681e8bd0
Gustavo C <137294905+GustavoH620@users.noreply.github.com>
2026-05-05 17:04:28 -0300

    Arquivo main com botões e chamada da função

---
+++ main.py
@@ -0,0 +1,45 @@
+import machine, ssd1306, time, menu_principal
+from machine import Pin, I2C
+from menu_principal import menu_principal
+
+
+
+btn1 = Pin(13, Pin.IN, Pin.PULL_UP)
+btn2 = Pin(12, Pin.IN, Pin.PULL_UP)
+btnEnter = Pin(14, Pin.IN, Pin.PULL_UP)
+estadoAnteriorEnter = 1
+tempoEnterAnterior = time.ticks_ms()
+enter = 1
+index = 0
+tempoAnterior = time.ticks_ms()
+
+while True:
+        valorBTN1 = btn1.value()
+        valorBTN2 = btn2.value()
+        valorBTNEnter = btnEnter.value()
+        tempoAtual = time.ticks_ms()
+        tempoEnterAtual = time.ticks_ms()
+        
+        if time.ticks_diff(tempoAtual, tempoAnterior) > 200:
+
+            if valorBTN1 == 0 and index > 0:
+                tempoAnterior = tempoAtual
+                index -= 1
+            if valorBTN2 == 0 and index < 3:
+                tempoAnterior = tempoAtual
+                index += 1
+        if valorBTNEnter == 0 and valorAnteriorEnter == 1:
+            enter = not enter
+            valorAnteriorEnter = 0
+            tempoEnterAnterior = tempoEnterAtual
+
+        if time.ticks_diff(tempoEnterAtual, tempoEnterAnterior) > 1000 and valorBTNEnter == 1:
+            valorAnteriorEnter = 1
+        
+        menu_principal(index, enter)
+
+            
+        #print(valorBTN1, " ", valorBTN2, " ", valorBTNEnter)
+        print("Enter: ", enter)
+
+    
